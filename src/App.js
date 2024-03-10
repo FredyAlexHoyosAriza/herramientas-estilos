d37a7c35b1682c87022f2330b5f07685aa832c71
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2024-03-09 19:19:53 -0500

    Se introducen componentes de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>Hola mundo</span>
+      <button type="button" className="btn btn-success">Success</button>
+
+      <div className="card">
+        <div className="card-header">
+          Featured
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <Card />
+
     </div>
   );
 }

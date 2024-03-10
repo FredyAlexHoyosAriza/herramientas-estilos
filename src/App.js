642766a8a8ee21b36b909813d9d47484ec5bed0d
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2024-03-09 23:04:03 -0500

    Se crea una clase personalizada tailwind y se guarda en index.css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-900">
-      Hello world!
-    </h1>
+    <div className='flex flex-col items-center justify-center'>
+      <div className='mx-2 bg-purple-400'>Elemento 1</div>
+      <div className='mx-2 bg-blue-600'>Elemento 2</div>
+      <div className='mx-2 bg-yellow-700'>Elemento 3</div>
+      <div className='mx-2 bg-green-800'>Elemento 4</div>
+      <button className='border-4 border-blue-900 p-2 text-red-400 font-bold rounded-lg shadow-lg hover:bg-violet-600 hover:cursor-pointer'>Boton 1</button>
+      <button className='boton'>Boton 2</button>
+    </div>
   );
 }
 

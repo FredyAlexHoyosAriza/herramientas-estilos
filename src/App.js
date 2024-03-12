3455ba351fa3dca58b6b64648a5b7201e4d69a6b
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2024-03-12 00:25:24 -0500

    Se introduce Sass: extension sass, import, variables, sass partials and modules and mixins

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className='fondoNegro'>Hola mundo, soy un ganador</div>
+      <div className='fondo1'>Hola mundo, soy un ganador, soy disciplinado</div>
+      <br />
+      <div className='fondo2'>Hola mundo, soy exitoso, estoy enfocado</div>
+      <br />
+      <div className='fondo3'>Proteger la familia</div>
+      <br />
+      <div className='fondo1'>Proteger a Susy</div>
     </div>
   );
 }

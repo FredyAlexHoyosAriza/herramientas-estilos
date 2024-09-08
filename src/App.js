0ff01cce60b73a308a6d7167077873901fd8c490
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2024-09-08 17:26:13 -0500

    Se agregan unos cambios minimos con proposito de practicas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className='mx-2 bg-blue-600'>Elemento 2</div>
       <div className='mx-2 bg-yellow-700'>Elemento 3</div>
       <div className='mx-2 bg-green-800'>Elemento 4</div>
-      <button className='border-4 border-blue-900 p-2 text-red-400 font-bold rounded-lg shadow-lg hover:bg-violet-600 hover:cursor-pointer'>Boton 1</button>
+      <button className='border-4 border-blue-900 p-2 text-red-400 font-bold rounded-lg shadow-lg bg-amber-950 hover:bg-violet-600 hover:cursor-pointer'>Boton 1</button>
       <button className='boton'>Boton 2</button>
     </div>
   );
